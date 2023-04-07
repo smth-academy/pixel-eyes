c30880fd31ffcf0ee93b1847526244eef815050d
MarcoDena <marcodena1993@gmail.com>
2023-04-07 13:02:42 +0200

    fixed puppeteer service

--- src/basic.command.ts
+++ src/basic.command.ts
@@ -10,7 +10,7 @@ import { PixelMatchingService } from './pixelmatching.service';
 @Command({ name: 'pixeleyes', description: 'TODO CREATE A DESCRIPTION' })
 export class BasicCommand extends CommandRunner {
   private readonly logger = new Logger(BasicCommand.name);
-
+ 
   constructor(
     private readonly diffedsService: DiffedsService,
     private readonly differsService: DiffersService,
@@ -18,11 +18,14 @@ export class BasicCommand extends CommandRunner {
     private readonly puppeteerService: PuppeteerService,
     private readonly pixelMatchingService: PixelMatchingService,
   ) {
-    super();
+    super(); 
   }
 
   async run(passedParam: string[], options?: any): Promise<any> {
     this.logger.log('Example command log');
     this.logger.error('IMPLEMENTS A COMMAND');
+    this.puppeteerService.takeScreenshot('https://viewer.stage.vesta3dmanager.it/?object_id=bottega_veneta&field=external_link')
+    .then((message) => console.log(message))
+    .catch((err) => console.error(err));
   }
 }
--- src/puppeter.service.ts
+++ src/puppeter.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import puppeteer, { Browser } from 'puppeteer';
 
 @Injectable()
 export class PuppeteerService {
-// TODO: @MarcoDena @Stefanoberka import a services to use a functions for Models
-  constructor() {}
+  // TODO: @MarcoDena @Stefanoberka import a services to use a functions for Models
+  constructor() { }
 
-  // TODO: @MarcoDena Create a functions to create a new browser instance and snapshooting a page
+  // TODO: @MarcoDena Create a functions to create a new browser instance and snapshooting a page  
+
+  public takeScreenshot(
+     url: string,
+     width: number = 1920,
+     height: number = 1080,
+     deviceScaleFactor: number = 1,
+  ): Promise<string> {
+    let browser: Browser;
+    return puppeteer
+      .launch({ headless: true })
+      .then((b: Browser) => {
+        browser = b;
+        return browser.newPage();
+      })
+      .then((page) => {
+        return Promise.all([
+          page.setViewport({
+            width: width,
+            height: height,
+            deviceScaleFactor: deviceScaleFactor,
+          }),
+          page.goto(url, { waitUntil: ['networkidle0', 'domcontentloaded'], timeout: 0 }),
+          page.waitForSelector('vesta-configurator'),
+        ]).then(() => page);
+      })
+      .then((page) => {
+        return page.evaluate(() => {
+          const shadowHost = document.querySelector('vesta-configurator');
+          const shadowRoot = shadowHost.shadowRoot;
+          return shadowRoot.querySelector('canvas')?.toDataURL();
+        })
+      } 
+      )
+      .then((result) => {
+        const imageData = result.replace(/^data:image\/\w+;base64,/, '');
+        require('fs').writeFileSync('screenshot.png', imageData, 'base64');
+        return 'Screenshot saved successfully.';
+      })
+      .catch((err) => {
+        console.error(err);
+        throw new Error('Error taking screenshot.');
+      })
+      .finally(() => {
+        browser.close();
+      });
+  }
+
 }
